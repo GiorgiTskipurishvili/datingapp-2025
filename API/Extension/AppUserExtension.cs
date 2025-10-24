cfabd0854356236abf72b0ed548ccb7309842fa4
GiorgiTskipurishvili <giorgitskipurishvili@gmail.com>
2025-10-24 17:18:43 +0400

    End of section 9

--- API/Extension/AppUserExtension.cs
+++ API/Extension/AppUserExtension.cs
@@ -13,6 +13,7 @@ namespace API.Extension
                 Id = user.Id,
                 DisplayName = user.DisplayName,
                 Email = user.Email,
+                ImageUrl= user.ImageUrl,
                 Token = tokenService.CreateToken(user)
             };
         }
